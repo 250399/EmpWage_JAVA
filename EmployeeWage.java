2f6498661b0f0f4dfdb0d77262536157d1f742b7
250399 <kunaldeshmukh2503@gmail.com>
2020-04-17 10:11:27 -0700

    ADDED: Calculating Daily Wage of Employee if present

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,12 +1,22 @@
 import java.lang.Math;
 class EmployeeWage{
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Program!");      
-        checkPresentorAbsent();
-    }
-    static void checkPresentorAbsent(){
-        String isPresent = (Math.round(Math.random())==1) ? "Present" : "Absent" ;
-        System.out.println("Employee is "+isPresent);    
-    }
-
+        final int fullTime = 1;
+		final int wagePerHour = 20;
+		final int fullDayHour = 8;
+		//variable
+        int dailyWage;
+        int dailyHours;
+		//computation
+		double employeeCheck = Math.round(Math.random());
+		if ( employeeCheck == fullTime ){
+			System.out.println("Employee is Present");
+			dailyHours = fullDayHour;
+		}
+		else {
+			System.out.println("Employee is Absent");
+			dailyHours = 0;
+		}
+		System.out.println("Employee daily wage is "+(wagePerHour * dailyHours));   }
+  
 }
