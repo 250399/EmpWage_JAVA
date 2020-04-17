30de832be0c724ba71795e79f9c8adafc798d979
250399 <kunaldeshmukh2503@gmail.com>
2020-04-17 10:48:28 -0700

    ADDED: Calculating monthly wage for employee

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -5,25 +5,25 @@ class EmployeeWage{
         final int PART_TIME = 2;
 		final int WAGE_PER_HOUR = 20;
 		final int FULL_DAY_HOUR = 8;
-		//variable
-        int dailyHours;
-		//computation
-        int employeeCheck = (int) Math.round(Math.random()*10)%3;
-        switch (employeeCheck){
-		case FULL_TIME:
-			System.out.println("Employee is Present");
-            dailyHours = FULL_DAY_HOUR;
-            break;
-		
-        case PART_TIME:
-			System.out.println("Employee is Present");
-            dailyHours=FULL_DAY_HOUR/2;
-            break;
-        default:
-			System.out.println("Employee is Absent");
-			dailyHours = 0;
+		final int WORKING_DAY_IN_MONTH=20;
+        int dailyHours=0;
+        int monthlyHours=0;
         
+        for(int i=0;i<WORKING_DAY_IN_MONTH;i++){       
+            dailyHours=0;
+            int employeeCheck = (int) Math.round(Math.random()*10)%3;
+            switch (employeeCheck){
+            case FULL_TIME:
+                dailyHours = FULL_DAY_HOUR+dailyHours;
+                break;
+            case PART_TIME:
+                dailyHours=FULL_DAY_HOUR/2+dailyHours;
+                break;
+            default:
+                
+        }
+        monthlyHours+=dailyHours;
     }
-		System.out.println("Employee daily wage is "+(WAGE_PER_HOUR * dailyHours));   }
+		System.out.println("Employee daily wage is "+(WAGE_PER_HOUR * monthlyHours ));   }
 
 }
