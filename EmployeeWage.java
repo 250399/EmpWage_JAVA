2e5b1a0a77e7a9135e91f0b0e0fb7363bf5e49b4
250399 <kunaldeshmukh2503@gmail.com>
2020-04-17 13:05:11 -0700

    Computing employee wages for multiple companies

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,22 +1,25 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 import java.lang.Math;
 class EmployeeWage{
+    static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
-		final int WAGE_PER_HOUR = 20;
-		final int FULL_DAY_HOUR = 8;
-		final int WORKING_DAY_IN_MONTH=20;
-        final int TOTAL_WORKING_HOURS=100;
-        
-        int empWage=computeEmployeeWage(FULL_DAY_HOUR,TOTAL_WORKING_HOURS,WORKING_DAY_IN_MONTH,WAGE_PER_HOUR);
-        System.out.println("Monthly employee age is: "+empWage);
+        Companies CompanyOne = new Companies(20,8,20,100,"Reliance");
+        computeEmployeeWage(CompanyOne.companyName, CompanyOne.FULL_DAY_HOUR,CompanyOne.TOTAL_WORKING_HOURS,CompanyOne.WORKING_DAY_IN_MONTH,CompanyOne.WAGE_PER_HOUR);
+        Companies CompanyTwo = new Companies(20,8,20,100,"BL");
+        computeEmployeeWage(CompanyTwo.companyName, CompanyTwo.FULL_DAY_HOUR,CompanyTwo.TOTAL_WORKING_HOURS,CompanyTwo.WORKING_DAY_IN_MONTH,CompanyTwo.WAGE_PER_HOUR);
     }
-    static int computeEmployeeWage(int FULL_DAY_HOUR,int TOTAL_WORKING_HOURS, int WORKING_DAY_IN_MONTH,int WAGE_PER_HOUR){
+
+    
+    static void computeEmployeeWage(String companyName, int FULL_DAY_HOUR,int TOTAL_WORKING_HOURS, int WORKING_DAY_IN_MONTH,int WAGE_PER_HOUR){
         int dailyHours=0;
         int monthlyHours=0;
         int hours=0;
         int days=0;
         final int FULL_TIME = 1;
         final int PART_TIME = 2;
-        while( hours <= TOTAL_WORKING_HOURS || days <= WORKING_DAY_IN_MONTH ){       
+        while( hours <= TOTAL_WORKING_HOURS && days <= WORKING_DAY_IN_MONTH ){       
             dailyHours=0;
             int employeeCheck = (int) Math.round(Math.random()*10)%3;
             switch (employeeCheck){
@@ -33,7 +36,23 @@ class EmployeeWage{
             days++;
             monthlyHours+=dailyHours;
         }
-    return (WAGE_PER_HOUR * monthlyHours ); 
+        System.out.println("Monthly employee wage for company "+companyName + "is: "+(WAGE_PER_HOUR*monthlyHours));
+ 
     }
 
+}
+
+class Companies{
+    final int WAGE_PER_HOUR ;
+    final int FULL_DAY_HOUR ;
+    final int WORKING_DAY_IN_MONTH;
+    final int TOTAL_WORKING_HOURS;
+    String companyName ;
+    Companies(int wagePerHour, int fullDayHours, int totalWorkingDayinMonth,int totalWorkingHours,String companyName){
+        this.WAGE_PER_HOUR=wagePerHour;
+        this.FULL_DAY_HOUR=fullDayHours;
+        this.WORKING_DAY_IN_MONTH=totalWorkingDayinMonth;
+        this.TOTAL_WORKING_HOURS=totalWorkingHours;
+        this.companyName=companyName;
+    }
 }
