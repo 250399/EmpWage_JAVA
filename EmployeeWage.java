73d6d86283da949459013a2cce6b3051422a6f7f
250399 <kunaldeshmukh2503@gmail.com>
2020-04-17 10:28:10 -0700

    Adding  part time employee and calculating its wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,22 +1,26 @@
 import java.lang.Math;
 class EmployeeWage{
     public static void main(String[] args) {
-        final int fullTime = 1;
-		final int wagePerHour = 20;
-		final int fullDayHour = 8;
+        final int FULL_TIME = 1;
+        final int PART_TIME = 2;
+		final int WAGE_PER_HOUR = 20;
+		final int FULL_DAY_HOUR = 8;
 		//variable
-        int dailyWage;
         int dailyHours;
 		//computation
-		double employeeCheck = Math.round(Math.random());
-		if ( employeeCheck == fullTime ){
+        double employeeCheck = Math.round(Math.random()*10)%3;
+		if ( employeeCheck == FULL_TIME ){
 			System.out.println("Employee is Present");
-			dailyHours = fullDayHour;
+			dailyHours = FULL_DAY_HOUR;
 		}
-		else {
+        else if( employeeCheck == PART_TIME ){
+			System.out.println("Employee is Present");
+            dailyHours=FULL_DAY_HOUR/2;
+        }
+        else {
 			System.out.println("Employee is Absent");
 			dailyHours = 0;
 		}
-		System.out.println("Employee daily wage is "+(wagePerHour * dailyHours));   }
-  
+		System.out.println("Employee daily wage is "+(WAGE_PER_HOUR * dailyHours));   }
+
 }
