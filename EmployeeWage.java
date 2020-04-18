dd9e97c48e5a2c928ad665c58ee5d2f03c61e2d8
250399 <kunaldeshmukh2503@gmail.com>
2020-04-17 18:28:47 -0700

    ADDED: Storing daily wage along with total wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -11,7 +11,7 @@ class EmployeeWage{
     
     public static void main(String[] args) {
         while(true){
-            System.out.println("1.Add company \n2.Renove company\n3.Compute Wages for companies \n4.Print Wages for companies \n5.Exit");
+            System.out.println("1.Add company \n2.Renove company\n3.Compute Wages for companies \n4.Print Wages for companies 5. display dailywage along with total wage\n6.Exit");
             int choice = sc.nextInt();
             switch(choice){
                 case 1: 
@@ -27,6 +27,9 @@ class EmployeeWage{
                     printCompanyWages();
                     break;
                 case 5:
+                    printDailyandTotalWage();
+                break;
+                case 6:
                     System.exit(1);
                 default:
                     System.out.println("Invalid choice");
@@ -34,6 +37,20 @@ class EmployeeWage{
         }
     }
 
+    static void printDailyandTotalWage(){
+        String name="";
+        System.out.println("Enter company name");
+        name=sc.next();
+        for(int index=0;index<companiesArray.size();index++){
+            if((companiesArray.get(index).companyName.toString().toLowerCase().equals(name.toLowerCase()))){
+                System.out.println("--------"+companiesArray.get(index).companyName.toString()+"--------");
+                for(int listIndex=0;listIndex<companiesArray.get(index).dailywages.size();listIndex++){
+                    int daily=companiesArray.get(index).dailywages.get(listIndex);
+                    System.out.println("daily wages are: "+daily+" and total wages is "+companiesArray.get(index).totalwage);
+                }
+            }
+        }
+    }
     static void removeCompany(){
         if(companiesArray.size()==0){
             System.out.println("No companies in array. Please add some first!");
@@ -91,6 +108,7 @@ class EmployeeWage{
         int monthlyHours=0;
         int hours=0;
         int days=0;
+        ArrayList<Integer> alist = new ArrayList<>();
         final int FULL_TIME = 1;
         final int PART_TIME = 2;
         while( hours <= TOTAL_WORKING_HOURS && days <= WORKING_DAY_IN_MONTH ){       
@@ -106,20 +124,29 @@ class EmployeeWage{
             default:
                 dailyHours+=0;                
             }
+            alist.add(dailyHours*WAGE_PER_HOUR);
             hours+=dailyHours;
             days++;
             monthlyHours+=dailyHours;
+        }
+        for(int listIndex=0;listIndex<companiesArray.size();listIndex++){
+            if(companiesArray.get(listIndex).companyName.toString().equals(companyName)){
+                companiesArray.get(listIndex).set(alist,(WAGE_PER_HOUR*monthlyHours));
+            }
         }
         companyWages.put(companyName,(WAGE_PER_HOUR*monthlyHours));
     }
 }
 
 class Companies{
+    HashMap<Integer,Integer> dailyAndTotalWage = new HashMap<>();
     final int WAGE_PER_HOUR ;
     final int FULL_DAY_HOUR ;
     final int WORKING_DAY_IN_MONTH;
     final int TOTAL_WORKING_HOURS;
-    String companyName ;
+    String companyName;
+    ArrayList<Integer> dailywages;
+    int totalwage;
     Companies(int wagePerHour, int fullDayHours, int totalWorkingDayinMonth,int totalWorkingHours,String companyName){
         this.WAGE_PER_HOUR=wagePerHour;
         this.FULL_DAY_HOUR=fullDayHours;
@@ -127,4 +154,8 @@ class Companies{
         this.TOTAL_WORKING_HOURS=totalWorkingHours;
         this.companyName=companyName;
     }
+    void set(ArrayList<Integer> dailyWages, int totalWage){
+        this.dailywages=dailyWages;
+        this.totalwage=totalWage;
+    }   
 }
